d14d6aab5f2a5d4d21883b95f5a43686a76c20bf
moralesboricua <moralesboricua@gmail.com>
2026-03-25 15:12:08 -0400

    updated

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import Todo from './components/Todo.jsx'
+import Title from './components/Title.jsx'
+import Modal from './components/Modal.jsx'
+
+
+
+function App() {
+  return (
+    <div>
+      <Title />
+    <div className="todo__wrapper">
+      <Todo />
+      <Todo />
+      <Todo />
+      <Todo />
+      <Todo />
+      <Todo />
+    </div>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/Todo.jsx
@@ -0,0 +1,13 @@
+import './Todo.css'
+
+
+function Todo() {
+    return (
+       <div className='todo'>
+        <h2>Finish Frontend Simplified</h2>
+        <button>Delete</button>
+      </div>
+    )
+}
+
+export default Todo
