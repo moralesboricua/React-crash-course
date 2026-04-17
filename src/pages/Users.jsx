8353499c41f9b402c67e5c22826a2206cf7631b2
moralesboricua <moralesboricua@gmail.com>
2026-04-17 09:34:38 -0400

    updated

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,15 +1,24 @@
 import { useParams } from "react-router-dom";
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import User from "../components/User.jsx";
+import axios from "axios";
 
 function Users() {
-    const { username } = useParams()
+    const { id } = useParams()
+    const [user, setUser] = useState({})
+
+
+    async function fetchUser() {
+        const { data } = await axios.get(`https://jsonplaceholder.typicode.com/${User}`)
+        console.log(data)
+        setUser(data)
+    }
 
     useEffect(() => {
-        //fetch(`https://api.instagram.com/${username}`)
+        fetchUser()
     }, [])
-
-    console.log((useParams()))
-    return <h1>{username}</h1>
+    return <h1>{id}</h1>
+            
 }
 
 export default Users
